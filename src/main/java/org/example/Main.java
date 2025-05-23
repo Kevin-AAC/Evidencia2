ef57ac959cbbf5cc5d9c438c3d32906190b7f61d
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-05-23 07:47:52 -0500

    Feat:Metodo para captura de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,39 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Persona> Personas = new ArrayList<>();
+
     public static void main(String[] args) {
+        CapturarDatos(Personas);
 
 
 
 
+    }
+    public static void CapturarDatos(ArrayList<Persona>){
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Ingresa tu nombre: ");
+            String nombre = scanner.nextLine();
+            System.out.println("Ingresa tu apellido: ");
+            String apellido = scanner.nextLine();
+            System.out.println("Ingresa tu genero: ");
+            String genero = scanner.nextLine();
+            System.out.println("Ingresa tu edad: ");
+            int edad = scanner.nextInt();
+            scanner.nextLine();
+            Personas.add(new Persona(nombre,apellido,genero,edad));
+
+            i++;
+        }
+        for (int j = 0; j< Personas.size();j++){
+            Persona p = Personas.get(j);
+            System.out.println("Nombre: " + p.getNombre() +  " Genero: "+ p.getGenero());
+        }
+        scanner.close();
     }
 }
