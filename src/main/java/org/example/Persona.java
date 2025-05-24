5e9980a159472c3318eb8d23390782c664beb187
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-05-23 20:28:48 -0500

    Feat:Se agregó los métodos getters.

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -13,5 +13,17 @@ public class Persona {
        this.edad = edad;
 
     }
+    public String getNombre(){return nombre;}
 
+    public void setNombre(String nombre){this.nombre = nombre;}
+
+    public String getApellido(){return apellido;}
+
+    public void setApellido(String apellido){this.apellido = apellido;}
+
+    public String getGenero(){return genero;}
+
+    public void setGenero(String genero){this.genero = genero;}
+
+    public int getEdad(){return edad;}
 }
